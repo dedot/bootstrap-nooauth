a430bb150ac7ce8bb8837c361b8de8a16e2e4f59
Stefan Schmitt <stefan.c.schmitt@gmail.com>
2013-08-20 14:32:31 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BootstrapNooauth::Application.config.secret_key_base = 'a9cb5082367fab9a8d6cd0647fa94a11b6dafa944a1e642ba5b053c622758d34e5951831e351382a688560d5cde5cc519569555c4a93e776712a331f7394c4bc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BootstrapNooauth::Application.config.session_store :cookie_store, key: '_bootstrap-nooauth_session'
